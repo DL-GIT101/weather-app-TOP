d7bbef420f3949800aae0990af575ff8629c3dbf
donlHenard <pastor.git101@gmail.com>
2024-03-13 18:46:31 +0800

    imported dotenv plugin

--- webpack.common.js
+++ webpack.common.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const Dotenv = require('dotenv-webpack');
 
  module.exports = {
    entry: {
@@ -17,6 +18,7 @@ const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
         filename: "[name].css",
         chunkFilename: "[id].css",
       }),
+     new Dotenv(),
    ],
    module: {
     rules: [
