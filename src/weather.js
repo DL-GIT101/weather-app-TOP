259fc8e5264de10c8732ecbdbb36044e6fe27566
donlHenard <pastor.git101@gmail.com>
2024-03-14 14:44:45 +0800

    Add func to get weather data based on city

---
+++ src/weather.js
@@ -0,0 +1,18 @@
+const key = process.env.WEATHER_API_KEY;
+
+const getWeatherData = async (city) => {
+    try {
+        const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${key}&q=${city}`, {mode: 'cors'});
+        const data = await response.json();
+
+        if(data.hasOwnProperty('error')){
+            throw new Error(data.error.message);
+        }else{
+            return data;
+        }
+    } catch (error) {
+        throw new Error('Failed to fetch weather data: ' + error.message);
+    }
+}
+
+export {getWeatherData};
