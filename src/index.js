f95a638bd992cb97f42fbb71b58e71692547c15a
donlHenard <pastor.git101@gmail.com>
2024-03-14 14:45:06 +0800

    made main into async and tried getting data

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import './style.css';
 import { createSearchBar } from './dom';
+import { getWeatherData } from './weather';
 
+const WeatherApp = async () => {
+    try {
+        const searchBar = createSearchBar();
 
-const WeatherApp = () => {
-
-    const searchBar = createSearchBar();
-
-    const body = document.querySelector('body');
-    body.append(searchBar);
+        const body = document.querySelector('body');
+        body.append(searchBar);
+    
+        const city = 'camiling';
+    
+        const weatherData = await getWeatherData(city);
+    
+        console.log(weatherData);
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 WeatherApp();
