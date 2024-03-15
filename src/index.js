5296b536aee7ab36ea8e9de58797bb5b0db1e905
donlHenard <pastor.git101@gmail.com>
2024-03-15 17:56:43 +0800

    Remove the doms only form was left

--- src/index.js
+++ src/index.js
@@ -9,20 +9,10 @@ const WeatherApp = async () => {
         main.className = "main";
         
         body.append(main);
+
+        const search = createSearchBar();
         
-        const weaterData = await getWeatherData("Mabiga");
-        const locationData = weaterData.location;
-        const currentWeatherData = weaterData.current;
-        
-        const cityName = createCityName(locationData);
-
-        const weatherCondition = createCurrWeatherCondition(currentWeatherData);
-
-        const windCondition = createWindCondition(currentWeatherData);
-
-        const otherDatas = createOtherData(currentWeatherData);
-
-        main.append(cityName,weatherCondition,windCondition,otherDatas);        
+        main.append(search);        
         
     } catch (error) {
         console.error(error);
